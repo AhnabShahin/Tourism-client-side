8eb239a1e23d570a31e497fd6186b4de2df612c4
shahin <shahinahnab@gmail.com>
2021-11-07 12:21:10 +0600

    firebase config hide

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAEIKKwsdqfIw9HViUnnDZIdUb6CQfij3E",
-  authDomain: "tourism-23.firebaseapp.com",
-  projectId: "tourism-23",
-  storageBucket: "tourism-23.appspot.com",
-  messagingSenderId: "639302690690",
-  appId: "1:639302690690:web:2e992f06d8b39ec771919b",
-  measurementId: "G-JL6PQN3RH1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, 
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 export default firebaseConfig;
